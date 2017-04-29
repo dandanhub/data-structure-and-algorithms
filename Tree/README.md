0facfd66247e0363f6fae45b757523525b79fbf2
dandanshi <dandanshi@live.cn>
2017-04-28 21:28:45 -0400

    Tree
    Tree

---
+++ Tree/README.md
@@ -0,0 +1,178 @@
+## 96. Unique Binary Search Trees
+Given n, how many structurally unique BST's (binary search trees) that store values 1...n?
+
+For example,
+Given n = 3, there are a total of 5 unique BST's.
+~~~~
+   1         3     3      2      1
+    \       /     /      / \      \
+     3     2     1      1   3      2
+    /     /       \                 \
+   2     1         2                 3
+~~~~
+
+#### Solution
+DP
+- n = 0: 1
+- n = 1: 1
+- n = 2: 2
+- n = 3: root = 1, 2; root = 2, 1; root = 3, 2;
+- n = 4: root = 1, 5; root = 2, 1 * 2 = 2; root = 3, 1 * 2 = 2; root = 4, 5
+- ...
+- n = k: root = i, dp[i-1] * dp[k-i]
+
+## 95. Unique Binary Search Trees II
+Given an integer n, generate all structurally unique BST's (binary search trees) that store values 1...n.
+
+For example,
+
+Given n = 3, your program should return all 5 unique BST's shown below.
+~~~~
+   1         3     3      2      1
+    \       /     /      / \      \
+     3     2     1      1   3      2
+    /     /       \                 \
+   2     1         2                 3
+~~~~
+
+#### Solution
+Add left and right tree recursively.
+The base case of recursive function is start equals to end.
+
+## 98. Validate Binary Search Tree
+Given a binary tree, determine if it is a valid binary search tree (BST).
+
+#### Solution
+- The first thought is to in-order traverse the tree and determine if the traversal result is in increasing order. Time complexity O(n) and space complexity O(n). Accepted.
+- An improved solution is to maintain a min and max value. The solution saves O(n) space. Use Long.MIN_VALUE, Long.MAX_VALUE as default low and up bound to avoid "overflow" issue.
+
+## 99. Recover Binary Search Tree
+Two elements of a binary search tree (BST) are swapped by mistake.
+
+Recover the tree without changing its structure.
+
+Note:
+A solution using O(n) space is pretty straight forward. Could you devise a constant space solution?
+
+#### Solution
+Use Morris Travesal (?) referring to [here](https://discuss.leetcode.com/topic/9305/detail-explain-about-how-morris-traversal-finds-two-incorrect-pointer).
+
+## 100. Same Tree
+Given two binary trees, write a function to check if they are equal or not.
+
+Two binary trees are considered equal if they are structurally identical and the nodes have the same value.
+
+#### Solution
+Easy. Pay attention to null handling.
+
+## 101. Symmetric Tree
+Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+
+#### Solution
+Recursively check the tree.
+
+
+
+
+## 112. Path Sum
+Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
+
+For example:
+Given the below binary tree and sum = 22,
+~~~
+              5
+             / \
+            4   8
+           /   / \
+          11  13  4
+         /  \      \
+        7    2      1
+~~~
+return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
+
+#### Solution
+Easy. Recursively solve the problem.
+
+## 113. Path Sum II
+Given a binary tree and a sum, find all root-to-leaf paths where each path's sum equals the given sum.
+
+For example:
+Given the below binary tree and sum = 22,
+~~~~
+              5
+             / \
+            4   8
+           /   / \
+          11  13  4
+         /  \    / \
+        7    2  5   1
+~~~~
+
+return
+~~~~
+[
+   [5,4,11,2],
+   [5,8,4,5]
+]
+~~~~
+
+#### Solution
+Backtracking based on Path Sum 1.
+
+## 124. Binary Tree Maximum Path Sum
+Given a binary tree, find the maximum path sum.
+
+For this problem, a path is defined as any sequence of nodes from some starting node to any node in the tree along the parent-child connections. The path must contain at least one node and does not need to go through the root.
+
+For example:
+Given the below binary tree,
+~~~~
+       1
+      / \
+     2   3
+~~~~
+Return 6.
+
+#### Solution
+We need to examine all possibilities and return the max sum.
+The trick is to use an instance variable for max sum.
+
+## 543. Diameter of Binary Tree
+Given a binary tree, you need to compute the length of the diameter of the tree. The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
+
+Example:
+Given a binary tree
+~~~~
+          1
+         / \
+        2   3
+       / \     
+      4   5    
+~~~~
+Return 3, which is the length of the path [4,2,1,3] or [5,2,1,3].
+Note: The length of path between two nodes is represented by the number of edges between them.
+
+#### Solution
+The thought is to traverse all possibilities, for a given node, there are two possibilities that its longest path is:
+1. when the path passes the node, length of the path = depth(left) + depth(right)
+2. when the path does not pass the node, length of the path = max(diameterOfBinaryTree(left), diameterOfBinaryTree(right))
+
+## 538. Convert BST to Greater Tree
+Given a Binary Search Tree (BST), convert it to a Greater Tree such that every key of the original BST is changed to the original key plus sum of all keys greater than the original key in BST.
+
+Example:
+~~~~
+Input: The root of a Binary Search Tree like this:
+              5
+            /   \
+           2     13
+
+Output: The root of a Greater Tree like this:
+             18
+            /   \
+          20     13
+~~~~
+
+#### Solution
+1. Method 1: I first used a naiive way to solve the issue. Trivial and prone to make mistakes.
+2. Method 2: Take advantage of BST, like doing in-order traversal (right - mid - left), by doing this, every node comes before is greater than current node.
---
+++ Tree/Tree.java
@@ -0,0 +1,241 @@
+public class Tree {
+  // 96. Unique Binary Search Trees
+  public int numTrees(int n) {
+        if (n == 0 || n == 1) return 1;
+        if (n == 2) return 2;
+
+        int[] count = new int[n + 1];
+        count[0] = 1;
+        count[1] = 1;
+        count[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                count[i] += count[j - 1] * count[i - j];
+            }
+        }
+
+        return count[n];
+    }
+
+  // 95. Unique Binary Search Trees II
+  public List<TreeNode> generateTrees(int n) {
+        if (n <= 0) return new ArrayList<TreeNode>();
+        return generateTreesHelper(1, n);
+  }
+
+  public List<TreeNode> generateTreesHelper(int start, int end) {
+      if (start > end) {
+          List<TreeNode> list = new ArrayList<TreeNode>();
+          list.add(null);
+          return list;
+      }
+
+      if (start == end) {
+          TreeNode root = new TreeNode(start);
+          List<TreeNode> list = new ArrayList<TreeNode>();
+          list.add(root);
+          return list;
+      }
+
+      List<TreeNode> list = new ArrayList<TreeNode>();
+      for (int i = start; i <= end; i++) {
+          List<TreeNode> leftnodes = generateTreesHelper(start, i - 1);
+          List<TreeNode> rightnodes = generateTreesHelper(i + 1, end);
+          for (TreeNode leftnode : leftnodes) {
+              for (TreeNode rightnode : rightnodes) {
+                  TreeNode root = new TreeNode(i);
+                  root.left = leftnode;
+                  root.right = rightnode;
+                  list.add(root);
+              }
+          }
+      }
+
+      return list;
+  }
+
+  // 98. Validate Binary Search Tree
+  // public boolean isValidBST(TreeNode root) {
+  //       List<Integer> ans = new ArrayList<Integer>();
+  //       inorder(root, ans);
+  //       for (int i = 1; i < ans.size(); i++) {
+  //           if (ans.get(i) <= ans.get(i-1)) {
+  //               return false;
+  //           }
+  //       }
+  //       return true;
+  // }
+  //
+  // private void inorder(TreeNode root, List<Integer> ans) {
+  //       if (root == null) {
+  //           return;
+  //       }
+  //       inorder(root.left, ans);
+  //       ans.add(root.val);
+  //       inorder(root.right, ans);
+  // }
+
+  // 98. Validate Binary Search Tree
+  public boolean isValidBST(TreeNode root) {
+        return isValidBSTHelper(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+  private boolean isValidBSTHelper(TreeNode root, long min, long max) {
+        if (root == null) {
+            return true;
+        }
+        if (root.val <= min || root.val >= max) {
+            return false;
+        }
+        return isValidBSTHelper(root.left, min, root.val) && isValidBSTHelper(root.right, root.val, max);
+  }
+
+  // 100. Same Tree
+  public boolean isSameTree(TreeNode p, TreeNode q) {
+       if (p == null && q == null) {
+           return true;
+       }
+       if ((p == null && q != null) || (p != null && q == null)) {
+           return false;
+       }
+       if (p.val != q.val) {
+           return false;
+       }
+       return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+   }
+
+   // 101. Symmetric Tree
+   public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isSymmetricHelper(root.left, root.right);
+    }
+
+    private boolean isSymmetricHelper(TreeNode root1, TreeNode root2) {
+        if (root1 == null && root2 == null) {
+            return true;
+        }
+        if ((root1 == null && root2 != null) || (root1 != null && root2 == null)) {
+            return false;
+        }
+        if (root1.val != root2.val) {
+            return false;
+        }
+        return isSymmetricHelper(root1.left, root2.right) && isSymmetricHelper(root1.right, root2.left);
+    }
+
+  // 543. Diameter of Binary Tree
+  public int diameterOfBinaryTree(TreeNode root) {
+        if (root == null || (root.left == null && root.right == null)) {
+            return 0;
+        }
+
+        int left = depth(root.left);
+        int right = depth(root.right);
+        int sum = left + right;
+
+        int leftDia = diameterOfBinaryTree(root.left);
+        int rightDia = diameterOfBinaryTree(root.right);
+        int maxDia = Math.max(leftDia, rightDia);
+
+        return Math.max(sum, maxDia);
+    }
+
+    private int depth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(depth(root.left), depth(root.right)) + 1;
+    }
+
+    // 113. Path Sum II
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        if (root == null) return ans;
+        searchPath(ans, root, new ArrayList<Integer>(), sum);
+        return ans;
+    }
+
+    public void searchPath(List<List<Integer>> list, TreeNode node, List<Integer> path, int sum) {
+        if (node.left == null && node.right == null && node.val == sum) {
+            List<Integer> ele = new ArrayList<Integer>(path);
+            ele.add(node.val);
+            list.add(ele);
+        }
+
+        // List<Integer> ele = new ArrayList<Integer>(path);
+        path.add(node.val);
+        if (node.left != null) searchPath(list, node.left, path, sum - node.val);
+        if (node.right != null) searchPath(list, node.right, path, sum - node.val);
+        path.remove(path.size() - 1);
+    }
+
+    // 124. Binary Tree Maximum Path Sum
+    private int ans = Integer.MIN_VALUE;
+
+    public int maxPathSum(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        maxPathSumHelper(root);
+        return ans;
+    }
+
+    private int maxPathSumHelper(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = maxPathSumHelper(root.left);
+        int right = maxPathSumHelper(root.right);
+        int temp = Math.max(left, 0) + Math.max(right, 0) + root.val;
+        ans = Math.max(temp, ans);
+        return Math.max(Math.max(left, right), 0) + root.val;
+    }
+
+    // 538. Convert BST to Greater Tree
+    // Method 1: Naiive Way
+    // public TreeNode convertBST(TreeNode root) {
+    //     if (root == null) {
+    //         return root;
+    //     }
+    //     convertBSTHelper(root, 0);
+    //     return root;
+    // }
+    //
+    // private void convertBSTHelper(TreeNode node, int parent) {
+    //     if (node == null) {
+    //         return;
+    //     }
+    //
+    //     int right = sumTree(node.right);
+    //     int sum = node.val + right + parent;
+    //     node.val = sum;
+    //
+    //     convertBSTHelper(node.left, sum);
+    //     convertBSTHelper(node.right, parent);
+    // }
+    //
+    // private int sumTree(TreeNode root) {
+    //     if (root == null) {
+    //         return 0;
+    //     }
+    //     int left = sumTree(root.left);
+    //     int right = sumTree(root.right);
+    //     int sum = left + right + root.val;
+    //     return sum;
+    // }
+
+    // 538. Convert BST to Greater Tree
+    private int sum = 0;
+    public TreeNode convertBST(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+        convertBST(root.right);
+        sum += root.val;
+        root.val = sum;
+        convertBST(root.left);
+        return root;
+    }
+}
